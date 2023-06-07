67da731d1388735dd142e509577301a047eb7c3b
ZenCodexs <djzengames@gmail.com>
2023-06-07 13:20:41 -0500

    mjk

--- app.js
+++ app.js
@@ -315,10 +315,8 @@ fastify.get('/cronTask', (request, reply) => {
               s3.upload(params, (err, data) => {
                 if (err) {
                   console.error('Error al cargar el archivo JSON en S3:', err);
-                  reply.code(500).send('Error al ejecutar la tarea programada');
                 } else {
                   console.log('Archivo JSON actualizado correctamente en S3:', data.Location);
-                  reply.send('Tarea programada ejecutada');
                 }
               });
             })
@@ -332,11 +330,14 @@ fastify.get('/cronTask', (request, reply) => {
         
         });
 
+        reply.code(200).send('Tarea programada ejecutada');
+
 
 
     })
     .catch(error => {
         console.error('Error al hacer la solicitud:', error);
+        reply.code(500).send('Error en la solicitud');
     });
   
 
