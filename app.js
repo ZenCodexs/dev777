0f05210c6fbd17a9cd9f2e3d01ee593393919d65
ZenCodexs <djzengames@gmail.com>
2023-06-07 01:32:08 -0500

    regresandolo a su estado funcional

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fastify = require('fastify')();
+const express = require('express');
 const cron = require('node-cron');
 const axios = require('axios');
 const XLSX = require('xlsx');
@@ -7,19 +8,32 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 require('dotenv').config({ path: './.env' });
 const AWS = require('aws-sdk');
-const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const s3 = new AWS.S3();
 
-const s3Client = new S3Client();
-const bucketName = process.env.BUCKET_NAME;
-
-const app = fastify.server;
-
+const app = express();
 const urlagrolalibertad = 'http://www.agrolalibertad.gob.pe/index.php?q=node/152';
 const serverUrl = process.env.SERVER_URL;
+
+
+const bucketName = process.env.BUCKET_NAME;
+const keyName = 'data.json';
 
 const options = {
   // Opciones de configuración del servidor Fastify
   port: 3000, // Reemplaza 3000 con el puerto que desees utilizar
+};
+
+
+// En lugar de leer localmente el archivo JSON, lo descargamos de S3
+const params1 = {
+  Bucket: bucketName,
+  Key: 'data.json',
+};
+
+
+let jsonData = {
+  añoactual: {},
+  añoanterior: {}
 };
 
 // Función para verificar si es un número
@@ -46,84 +60,48 @@ function findCellValuePosition(sheet, targetValue) {
   return -1; // El valor no se encontró en la hoja
 }
 
-fastify.get('/data', async (request, reply) => {
-  try {
+fastify.get('/data', (request, reply) => {
+  const params1 = {
+    Bucket: bucketName,
+    Key: 'data.json',
+  };
 
-    //=============================================================================================
-
-async function borrarContenidoJSON() {
-  try {
-    const fileName = "data.json";
-    // Descargar el archivo JSON desde S3
-    const params = { Bucket: bucketName, Key: fileName };
-    const { Body } = await s3Client.send(new GetObjectCommand(params));
-    const jsonDataString = Body.toString("utf-8");
-
-    // Parsear el contenido del archivo JSON
-    let jsonData;
-    try {
-      jsonData = JSON.parse(jsonDataString);
-    } catch (parseError) {
-      console.error("Error al analizar el JSON descargado:", parseError);
-      console.log("Contenido del JSON:", jsonDataString);
-      return;
+  s3.getObject(params1, (err, data) => {
+    if (err) {
+      console.error('Error al descargar el archivo JSON de S3:', err);
+      reply.status(500).send({ error: 'Error al obtener los datos' });
+    } else {
+      const jsonDataString = data.Body.toString();
+      const jsonData = JSON.parse(jsonDataString);
+      reply.send(jsonData);
     }
-
-    // Borrar el contenido existente en el objeto JSON
-    jsonData = {};
-
-    // Convertir el objeto JSON modificado a formato de cadena
-    const updatedJsonDataString = JSON.stringify(jsonData, null, 2);
-
-    // Sobrescribir el archivo JSON en S3 con el contenido actualizado
-    const putParams = {
-      Bucket: bucketName,
-      Key: fileName,
-      Body: updatedJsonDataString,
-      ContentType: "application/json",
-    };
-    await s3Client.send(new PutObjectCommand(putParams));
-
-    console.log("Contenido del archivo JSON borrado exitosamente.");
-  } catch (error) {
-    console.error("Error al borrar el contenido del archivo JSON:", error);
-  }
-}
-
-borrarContenidoJSON();
-
-    //=============================================================================================
-    const params = {
-      Bucket: bucketName,
-      Key: 'data.json',
-    };
-
-    const data = await s3Client.send(new GetObjectCommand(params));
-    const jsonDataString = data.Body.toString();
-    const jsonData = JSON.parse(jsonDataString);
-
-    reply.send(jsonData);
-  } catch (error) {
-    console.error('Error al descargar el archivo JSON de S3:', error);
-    reply.status(500).send({ error: 'Error al obtener los datos' });
-  }
+  });
 });
 
-fastify.get('/cronTask', async (request, reply) => {
-  await fetchDataAndSaveToJson();
-  reply.send('Tarea programada ejecutada');
+fastify.get('/cronTask', (request, reply) => {
+  fetchDataAndSaveToJson();
+  //reply.send('Tarea programada ejecutada');
 });
+
+const port = 3000;
+
+
+
 
 fastify.listen(options, (err, address) => {
   if (err) {
     console.error('Error al iniciar el servidor Fastify:', err);
     process.exit(1);
   }
-  console.log(`Servidor Fastify iniciado en el puerto ${options.port}`);
+  console.log(`Servidor Fastify iniciado en el puerto ${port}`);
 });
 
 // Obtener los datos y guardarlos en un archivo JSON
-const fetchDataAndSaveToJson = async () => {
+const fetchDataAndSaveToJson = () => {
+
+    const sheetName = 'FEB';
+  //const startCell = 'B7';
+  //const endCell = 'S7';
   const currentDate = new Date();
   const currentMonth = currentDate.getMonth();
   const months = [
@@ -132,60 +110,250 @@ const fetchDataAndSaveToJson = async () => {
   ];
   const meses = months.slice(0, currentMonth + 1);
 
-  try {
-    const response = await axios.get(urlagrolalibertad);
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const table = $('table.tabla', '#block-system-main').first();
-    const sheet = XLSX.utils.table_to_sheet(table);
-    const jsonData = {};
+  //====================================================================================================
+  axios.get(urlagrolalibertad)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
 
-    meses.forEach((mes) => {
-      const row = findCellValuePosition(sheet, mes);
+        // Buscar la tabla con el id "attachments"
+        const attachmentsTable = $('#attachments');
 
-      if (row !== -1) {
-        const fechaCell = `A${row}`;
-        const fecha = sheet[fechaCell]?.v;
-        const precios = {};
+        // Obtener los enlaces de la tabla de "attachments"
+        const links = [];
+        attachmentsTable.find('tbody tr').each((index, element) => {
+        const linkElement = $(element).find('a');
+        const link = linkElement.attr('href');
+        links.push(link);
+        });
 
-        for (let col = 1; col <= 6; col++) {
-          const productoCell = `${XLSX.utils.encode_col(col)}${row}`;
-          const precioCell = `${XLSX.utils.encode_col(col)}${row + 1}`;
-          const producto = sheet[productoCell]?.v;
-          const precio = sheet[precioCell]?.v;
+        // Obtener los dos últimos enlaces
+        const url = links[links.length - 1];
+        const urlAñoAnterior = links[links.length - 2];
 
-          if (producto && precio && isNumeric(precio)) {
-            precios[producto] = precio;
-          }
+        //console.log('URL actual del año:', urlactualyear);
+        //console.log('URL del año anterior:', previousyear);
+
+
+        axios.get(url, { responseType: 'arraybuffer' })
+        .then(response => {
+        const workbook = XLSX.read(response.data, { type: 'buffer' });
+
+        const jsonData = {
+            añoactual: {},
+            añoanterior: {}
+        };
+
+
+
+        
+
+
+
+
+        for (let i = 0; i < meses.length; i++) {
+            const sheetName = meses[i];
+            const sheet = workbook.Sheets[sheetName];
+            //====================================================================================================
+            const viruRow = findCellValuePosition(sheet, 'VIRU');
+            const startCell = `B${viruRow - 2}`;
+            const endCell = `S${viruRow - 2}`;
+            //====================================================================================================
+            const range = XLSX.utils.decode_range(`${startCell}:${endCell}`);
+            let count = 0;
+
+            for (let R = range.s.r; R <= range.e.r; ++R) {
+            for (let C = range.s.c; C <= range.e.c; ++C) {
+                const cellAddress = XLSX.utils.encode_cell({ r: R, c: C });
+                const cell = sheet[cellAddress];
+
+                if (cell && cell.v) {
+                const cellValue = cell.v;
+
+                if (isNumeric(cellValue)) {
+                    count++;
+                }
+                }
+            }
+            }
+
+            const dias = [];
+            for (let i = 1; i <= count; i++) {
+            const cell = sheet[XLSX.utils.encode_cell({ r: viruRow-3, c: i })];
+            if (cell && cell.v) {
+                dias.push(cell.v);
+            }
+            }
+
+            const preciosValleA9 = [];
+            for (let i = 1; i <= count; i++) {
+            const cell = sheet[XLSX.utils.encode_cell({ r: viruRow-1, c: i })];
+            if (cell && cell.v) {
+                preciosValleA9.push({ fecha: dias[i - 1], precio: cell.v });
+            }
+            }
+
+            const preciosValleA10 = [];
+            for (let i = 1; i <= count; i++) {
+            const cell = sheet[XLSX.utils.encode_cell({ r: viruRow+1, c: i })];
+            if (cell && cell.v) {
+                preciosValleA10.push({ fecha: dias[i - 1], precio: cell.v });
+            }
+            }
+
+            const preciosValleA11 = [];
+            for (let i = 1; i <= count; i++) {
+            const cell = sheet[XLSX.utils.encode_cell({ r: 10, c: i })];
+            if (cell && cell.v) {
+                preciosValleA11.push({ fecha: dias[i - 1], precio: cell.v });
+            }
+            }
+
+            jsonData.añoactual[meses[i]] = {
+            VIRU: preciosValleA9,
+            CHICAMA: preciosValleA10,
+            MOCHE: preciosValleA11
+            };
         }
 
-        jsonData[fecha] = precios;
-      }
+        const añoanterior = {};
+        //const urlAñoAnterior = 'http://www.agrolalibertad.gob.pe/sites/default/files/PRECIOS_MAIZ_AMARILLO_DURO_VIRU-ASCOPE_ANO_2022_0.xlsx';
+        //const startCellAñoAnterior = 'B7';
+        //const endCellAñoAnterior = 'S7';
+        const monthsAñoAnterior = ['ENE', 'FEB', 'MAR', 'ABR', 'MAY', 'JUN', 'JUL', 'AGO', 'SET', 'OCT', 'NOV', 'DIC'];
+
+        axios.get(urlAñoAnterior, { responseType: 'arraybuffer' })
+        .then(response => {
+            const workbook = XLSX.read(response.data, { type: 'buffer' });
+
+
+            // Objeto para almacenar los datos de los meses
+            //const jsonData = {};
+
+            // Iterar sobre cada mes
+            for (let i = 0; i < monthsAñoAnterior.length; i++) {
+                const sheetName = monthsAñoAnterior[i];
+                const sheet = workbook.Sheets[sheetName];
+                //====================================================================================================
+                const viruRow = findCellValuePosition(sheet, 'VIRU');
+                const startCellAñoAnterior = `B${viruRow - 2}`;
+                const endCellAñoAnterior  = `S${viruRow - 2}`;
+                //====================================================================================================
+
+
+
+            // Verificar si la hoja existe en el archivo
+            if (sheet) {
+                // Obtener el rango de celdas
+                const range = XLSX.utils.decode_range(`${startCellAñoAnterior}:${endCellAñoAnterior}`);
+
+                // Contador para almacenar la cantidad de celdas con números
+                let count = 0;
+
+                // Iterar sobre las celdas en el rango
+                for (let R = range.s.r; R <= range.e.r; ++R) {
+                for (let C = range.s.c; C <= range.e.c; ++C) {
+                    // Construir el nombre de la celda
+                    const cellAddress = XLSX.utils.encode_cell({ r: R, c: C });
+
+                    // Obtener la celda
+                    const cell = sheet[cellAddress];
+
+                    // Verificar si la celda existe y tiene un valor
+                    if (cell && cell.v) {
+                    // Obtener el valor de la celda
+                    const cellValue = cell.v;
+
+                    // Verificar si el valor de la celda es un número
+                    if (isNumeric(cellValue)) {
+                        count++;
+                    }
+                    }
+                }
+                }
+
+                // Obtener los días desde las celdas C7 a N7
+                const dias = [];
+                for (let i = 1; i <= count; i++) {
+                const cell = sheet[XLSX.utils.encode_cell({ r: viruRow-3, c: i })];
+                if (cell && cell.v) {
+                    dias.push(cell.v);
+                }
+                }
+
+                // Obtener los precios para el valle A9 desde las celdas C9 a N9
+                const preciosValleA9 = [];
+                for (let i = 1; i <= count; i++) {
+                const cell = sheet[XLSX.utils.encode_cell({ r: viruRow-1, c: i })];
+                if (cell && cell.v) {
+                    preciosValleA9.push({ fecha: dias[i - 1], precio: cell.v });
+                }
+                }
+
+                // Obtener los precios para el valle A10 desde las celdas C10 a N10
+                const preciosValleA10 = [];
+                for (let i = 1; i <= count; i++) {
+                const cell = sheet[XLSX.utils.encode_cell({ r: viruRow, c: i })];
+                if (cell && cell.v) {
+                    preciosValleA10.push({ fecha: dias[i - 1], precio: cell.v });
+                }
+                }
+
+                // Obtener los precios para el valle A11 desde las celdas C11 a N11
+                const preciosValleA11 = [];
+                for (let i = 1; i <= count; i++) {
+                const cell = sheet[XLSX.utils.encode_cell({ r: viruRow+1, c: i })];
+                if (cell && cell.v) {
+                    preciosValleA11.push({ fecha: dias[i - 1], precio: cell.v });
+                }
+                }
+
+                // Almacenar los datos en el objeto jsonData
+                jsonData.añoanterior[months[i]] = {
+                VIRU: preciosValleA9,
+                CHICAMA: preciosValleA10,
+                MOCHE: preciosValleA11
+                };
+            }
+            }
+
+            //jsonData.añoanterior = añoanterior;
+
+            cache.put('data', jsonData, 30 * 60 * 1000);
+
+            const jsonDataString = JSON.stringify(jsonData, null, 2);
+            const params = {
+              Bucket: bucketName,
+              Key: 'data.json',
+              Body: jsonDataString,
+            };
+
+              s3.upload(params, (err, data) => {
+                if (err) {
+                  console.error('Error al cargar el archivo JSON en S3:', err);
+                } else {
+                  console.log('Archivo JSON actualizado correctamente en S3:', data.Location);
+                }
+              });
+            })
+            .catch(error => {
+            console.log('Error al descargar el archivo del año anterior:', error);
+           
+            });
+        })
+        .catch(error => {
+        console.log('Error al descargar el archivo actual:', error);
+        
+        });
+
+
+
+    })
+    .catch(error => {
+        console.error('Error al hacer la solicitud:', error);
     });
-
-    // Guardar el archivo JSON en S3
-    const jsonDataString = JSON.stringify(jsonData);
-
-    const params = {
-      Bucket: bucketName,
-      Key: 'data.json',
-      Body: jsonDataString,
-      ContentType: 'application/json',
-    };
-
-    await s3Client.send(new PutObjectCommand(params));
-    console.log('Datos guardados en S3');
-  } catch (error) {
-    console.error('Error al obtener los datos:', error);
-  }
+  
 };
 
-// Programar tarea cron para ejecutar cada día a las 12:00 AM
-cron.schedule('0 0 * * *', async () => {
-  await fetchDataAndSaveToJson();
-});
-
-// Ejecutar la función para obtener los datos iniciales al iniciar el servidor
-fetchDataAndSaveToJson().catch((error) => {
-  console.error('Error al obtener los datos iniciales:', error);
-});
+// Ejecutar la función fetchDataAndSaveToJson al iniciar el servidor
+fetchDataAndSaveToJson();
